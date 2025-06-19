8dfd147b5b680d73fd63e2442e57b0aa6828022d
Yadavinder Berry <berryyadu@gmail.com>
2025-06-19 21:21:50 +0530

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
-gen = ImageDataGenerator(rescale=1./255)
-data = gen.flow_from_directory("dataset/train", target_size=(224, 224))
-
-# OR
-# data = gen.flow_from_directory("dataset/train", target_size=(224,224))
